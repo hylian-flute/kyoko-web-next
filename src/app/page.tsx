735a9c4671464d9f4a7e7878895ec9232fb77fd6
hylian-flute <hylian_flute@yahoo.co.jp>
2024-08-25 19:20:38 +0900

    デザイン変更に対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,41 +32,137 @@ export default function Home() {
           <br />
           お気軽にお問い合わせください。
         </p>
-        <ul className={style.linkList}>
-          <li>
-            <Link href="/">
-              <Image src="/image/thumbnails/business_card.png" alt="" width={227} height={283} />
-              <div>名刺</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src="/image/thumbnails/shop_card.png" alt="" width={227} height={283} />
-              <div>正方形ショップカード</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src="/image/thumbnails/shop_sticker.png" alt="" width={227} height={283} />
-              <div>ショップシール</div>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src="/image/thumbnails/diploma.png" alt="" width={227} height={283} />
-              <div>ディプロマ</div>
-            </Link>
-          </li>
-        </ul>
+        <div>
+          <Link className={style.linkCard} href="/">
+            <Image
+              className={style.image}
+              src="/image/thumbnails/business_card.png"
+              alt=""
+              width={112}
+              height={90}
+            />
+            <div className={style.text}>
+              <div className={style.cardTitle}>名刺</div>
+              <div className={style.cardSubtitle}>100枚&emsp;4000円</div>
+              <div className={style.cardDescription}>
+                ミンネでは少量でも販売
+                <br />
+                2500円～
+              </div>
+            </div>
+          </Link>
+          <Link className={style.linkCard} href="/">
+            <Image
+              className={style.image}
+              src="/image/thumbnails/shop_card.png"
+              alt=""
+              width={114}
+              height={86}
+            />
+            <div className={style.text}>
+              <div className={[style.cardTitle, style.narrow].join(" ")}>正方形ショップカード</div>
+              <div className={style.cardSubtitle}>80枚&emsp;3500円</div>
+              <div className={style.cardDescription}>SIZE:50mm/40mm</div>
+            </div>
+          </Link>
+          <Link className={style.linkCard} href="/">
+            <Image
+              className={style.image}
+              src="/image/thumbnails/shop_sticker.png"
+              alt=""
+              width={127}
+              height={97}
+            />
+            <div className={style.text}>
+              <div className={style.cardTitle}>ショップシール</div>
+              <div className={style.cardSubtitle}>100枚&emsp;3110円～</div>
+              <div className={style.cardDescription}>
+                正円シール&nbsp;/&nbsp;楕円シール
+                <br />
+                正方形シール
+                <br />
+                透明PET用紙シール
+              </div>
+            </div>
+          </Link>
+          <Link className={style.linkCard} href="/">
+            <Image
+              className={style.image}
+              src="/image/thumbnails/diploma.png"
+              alt=""
+              width={128}
+              height={90}
+            />
+            <div className={style.text}>
+              <div className={style.cardTitle}>ディプロマ</div>
+              <div className={style.cardSubtitle}>
+                デザイン料&emsp;5000円
+                <br />
+                <span className={style.small}>※&nbsp;初回のみ</span>
+              </div>
+              <div className={style.cardDescription}>
+                印刷代&nbsp;1名分500円～
+                <br />
+                講座名&nbsp;/&nbsp;生徒様名&nbsp;/&nbsp;取得日
+              </div>
+            </div>
+          </Link>
+        </div>
+        {
+          // <ul className={style.linkList}>
+          //   <li>
+          //     <Link href="/">
+          //       <Image src="/image/thumbnails/business_card.png" alt="" width={227} height={283} />
+          //       <div>名刺</div>
+          //     </Link>
+          //   </li>
+          //   <li>
+          //     <Link href="/">
+          //       <Image src="/image/thumbnails/shop_card.png" alt="" width={227} height={283} />
+          //       <div>正方形ショップカード</div>
+          //     </Link>
+          //   </li>
+          //   <li>
+          //     <Link href="/">
+          //       <Image src="/image/thumbnails/shop_sticker.png" alt="" width={227} height={283} />
+          //       <div>ショップシール</div>
+          //     </Link>
+          //   </li>
+          //   <li>
+          //     <Link href="/">
+          //       <Image src="/image/thumbnails/diploma.png" alt="" width={227} height={283} />
+          //       <div>ディプロマ</div>
+          //     </Link>
+          //   </li>
+          // </ul>
+        }
         <div className={style.socialList}>
           <a href="/">
-            <Image className={style.instagram} src="/image/social/instagram.png" alt="Instagram" width={52} height={52} />
+            <Image
+              className={style.instagram}
+              src="/image/social/instagram.png"
+              alt="Instagram"
+              width={52}
+              height={52}
+            />
           </a>
           <a href="/">
-            <Image className={style.ameba} src="/image/social/ameba.png" alt="アメーバブログ" width={97} height={112} />
+            <Image
+              className={style.ameba}
+              src="/image/social/ameba.png"
+              alt="アメーバブログ"
+              width={97}
+              height={112}
+            />
           </a>
           <a href="/">
-            <Image className={style.minne} src="/image/social/minne.png" alt="minne" width={120} height={50} />
+            <Image
+              className={style.minne}
+              src="/image/social/minne.png"
+              alt="minne"
+              width={120}
+              height={50}
+            />
           </a>
         </div>
       </main>
