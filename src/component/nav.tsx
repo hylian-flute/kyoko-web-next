e306a0148ba43015e645d4fbeca2439be7f7b3c1
hylian-flute <hylian_flute@yahoo.co.jp>
2025-04-18 23:23:26 +0900

    諸々修正

--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -20,12 +20,17 @@ export default function Nav({ className }: Props) {
         ></Image>
       </div>
       <ul className={style.linkList}>
-        <li><Link href="/business-card/">名刺</Link></li>
-        <li><ExternalLink href="https://ameblo.jp/nanarin-0328/entry-11715685486.html">スタンプカード</ExternalLink></li>
-        <li><Link href="/shop-card/">正方形ショップカード</Link></li>
-        <li><ExternalLink href="https://ameblo.jp/nanarin-0328/entry-12593038491.html">タグ</ExternalLink></li>
-        <li><Link href="/shop-sticker/">シール</Link></li>
-        <li><Link href="/diploma/">ディプロマ</Link></li>
+        <li>
+          <span className={style.head}>テンプレートデザイン一覧ページ</span>
+          <ul className={style.linkList}>
+            <li><Link href="/business-card/">名刺</Link></li>
+            <li><ExternalLink href="https://ameblo.jp/nanarin-0328/entry-11715685486.html">スタンプカード</ExternalLink></li>
+            <li><Link href="/shop-card/">正方形ショップカード</Link></li>
+            <li><ExternalLink href="https://ameblo.jp/nanarin-0328/entry-12593038491.html">タグ</ExternalLink></li>
+            <li><Link href="/shop-sticker/">シール</Link></li>
+            <li><Link href="/diploma/">ディプロマ</Link></li>
+          </ul>
+        </li>
         <li><Link href="/flyer/">フライヤー</Link></li>
         <li><Link href="/leaflet/">リーフレット</Link></li>
         <li><Link href="/customer/">お客様からの画像</Link></li>
